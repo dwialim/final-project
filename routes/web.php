6bf110ed0280323fe10905821c8a9192f10a11d6
Dwi Alim <dwialim27@gmail.com>
2020-07-09 21:54:57 +0700

    update route controller

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,20 @@ Route::get('/', 'HomeController@redirect');
 
 Route::group(['middleware' => 'auth'], function(){
 	Route::get('/stacloverload','StackController@index')->name('dashboard');
+
+	Route::get('/stacloverload/create','QuestionController@create')->name('createquest');
+	Route::post('/stacloverload','QuestionController@store')->name('savequest');
+
+	Route::get('/detail/{question_id}','QuestionController@index')->name('detail');
+	Route::get('/detail/{question_id}/questedit','QuestionController@edit')->name('editquest');
+	Route::put('/detail/{question_id}/questedit','QuestionController@update')->name('updatequest');
+	Route::delete('/detail/{question_id}question','QuestionController@destroy')->name('deletequest');
+
+	Route::post('/detail/{question_id}','AnswerController@store')->name('saveanswer');
+	Route::get('/detail/{question_id}/ansedit','AnswerController@edit')->name('editanswer');
+	Route::put('/detail/{question_id}/ansedit','AnswerController@update')->name('updateanswer');
+	Route::delete('/detail/{question_id}answer','AnswerController@destroy')->name('deleteanswer');
+
 });
 
 Auth::routes();
