5e15d2d830e030c4268420441315683e1b382be1
akbarsaladin36 <asong36@gmail.com>
2020-07-11 10:59:55 +0700

    your changes

--- app/Answer.php
+++ app/Answer.php
@@ -2,9 +2,20 @@
 
 namespace App;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 
-class Answer extends Model
+class AnswerModel
 {
-    //
+    protected $table = 'answers';
+    public static function save($data, $user_id, $questid) {
+        unset($data['_token']);
+        $data['created_at'] = Carbon::now();
+        $data['updated_at'] = NULL;
+        $data['question_id'] = $questid;
+        $data['user_id'] = $user_id;
+        dd ($data);die();
+        $simpan = DB::table('answers')->insert($data);
+        return $simpan;
+    }
 }
--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -3,15 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Answer;
+use App\Question;
+use DateTime;
 
 class AnswerController extends Controller
 {
-    public function index($id){
-        // 
+    public function index($id)
+    {
+        $data = Answer::get_answer($id);
+        $data2 = Question::get($id);
+        return view('detail.{question_id}.answer', compact('data', 'data2'));
     }
 
-    public function store(Request $request){
-    	// 
+    public function store(Request $req, $questid)
+    {
+        $user_id = Auth::id();
+        $simpan = AnswerModel::save($req->all(),$user_id,$questid);
+        return redirect('detail/'. $questid);
     }
 
     public function edit($id){
